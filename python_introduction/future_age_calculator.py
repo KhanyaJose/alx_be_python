d185867a0466f9850ff3e2a7dee14a3411e671fd
KhanyaJose <156600985+KhanyaJose@users.noreply.github.com>
2025-11-14 15:00:02 +0200

    Add files via upload

---
+++ python_introduction/future_age_calculator.py
@@ -0,0 +1,6 @@
+# Future Age Calculator
+
+current_age = int(input("How old are you? "))
+future_age = current_age + (2050 - 2023)
+
+print(f"In 2050, you will be {future_age} years old.")
---
+++ python_introduction/hours_to_seconds.py
@@ -0,0 +1,6 @@
+# Hours to Seconds Converter
+
+hours = 2
+seconds = hours * 3600
+
+print(f"{hours} hour(s) is {seconds} seconds.")
